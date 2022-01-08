6ef6853ce4f2f74ebca9e39d1fbb8ee2126eeccd
zhangjinjie <zhangjinjie@luojilab.com>
2022-01-08 12:57:58 +0800

    feat: 测试template vetur

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,6 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
-  </div>
+  <img alt="Vue logo" src="../assets/logo.png" />
+  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
 </template>
 
 <script lang="ts">
