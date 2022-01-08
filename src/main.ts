674f8b1ff49647e27f80dd937ee9f1fd548d1dec
zhangjinjie <zhangjinjie@luojilab.com>
2022-01-08 14:54:40 +0800

    feat: 引入element-plus；

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,13 @@
 import { createApp } from "vue";
+import ElementPlus from "element-plus";
+
+import "element-plus/lib/theme-chalk/index.css";
+import locale from "element-plus/lib/locale/lang/zh-cn";
+
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
-createApp(App).use(store).use(router).mount("#app");
+const app = createApp(App);
+
+app.use(ElementPlus, { locale }).use(store).use(router).mount("#app");
